053904a0721fc91e21dab3b7223420407376f96d
Ollie Kemp <oliverkemp1999@gmail.com>
2022-07-29 10:09:01 +0100

    Added function documentation

--- src/add_cpp.cpp
+++ src/add_cpp.cpp
@@ -1,6 +1,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//' Add Two Numbers
+//'
+//' Return the sum of two numbers.
+//' @param x A number as a double
+//' @param y A number as a double
+//' @returns value The sum of the two inputs
+//' @export
 // [[Rcpp::export]]
 double add_cpp(double x, double y) {
   double value = x + y;
--- src/sumC.cpp
+++ src/sumC.cpp
@@ -1,7 +1,12 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-
+//' Adds a vector of numbers
+//'
+//' Return the sum of all numbers in a numeric vector.
+//' @param x A numeric vector
+//' @returns value The sum of the numbers
+//' @export
 // [[Rcpp::export]]
 double sumC(NumericVector x) {
   int n = x.size();
